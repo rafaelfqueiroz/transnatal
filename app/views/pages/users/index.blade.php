08fb74fd4a4acfedd781d6e172638d1380282a49
Rafael Fernandes <rfqueroz.91@gmail.com>
2014-05-12 20:25:09 -0300

    index de usuarios

--- app/views/pages/users/index.blade.php
+++ app/views/pages/users/index.blade.php
@@ -25,7 +25,7 @@
 	        	<div class="col-md-10">
                     <div class="box">
                         <div class="box-header">
-                            <h3 class="box-title">Listagem de clientes</h3>
+                            <h3 class="box-title">Listagem de usuários</h3>
                         </div>
                         <div class="box-body table-responsive">
                             <div id="example1_wrapper" class="dataTables_wrapper form-inline" role="grid">
